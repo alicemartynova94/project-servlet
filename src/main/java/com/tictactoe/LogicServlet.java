0c11bade8703baa77885d216a55060acc1caa3b1
alicemartynova94 <alicemartynova94@gmail.ru>
2023-07-05 15:07:39 +0300

    add extractField method, new game logic to doGet method

--- src/main/java/com/tictactoe/LogicServlet.java
+++ src/main/java/com/tictactoe/LogicServlet.java
@@ -4,13 +4,22 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "LogicServlet", value = "/logic")
 public class LogicServlet extends javax.servlet.http.HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession currentSession = req.getSession();
+        Field field = extractField(currentSession);
         int index = getSelectedIndex(req);
+        
+        field.getField().put(index, Sign.CROSS);
+        List<Sign> data = field.getFieldData();
+        currentSession.setAttribute("data", data);
+        currentSession.setAttribute("field", field);
         resp.sendRedirect("/index.jsp");
     }
 
@@ -18,5 +27,14 @@ public class LogicServlet extends javax.servlet.http.HttpServlet {
         String click = request.getParameter("click");
         boolean isNumeric = click.chars().allMatch(Character::isDigit);
         return isNumeric ? Integer.parseInt(click) : 0;
+    }
+
+    private Field extractField(HttpSession currentSession) {
+        Object fieldAttribute = currentSession.getAttribute("field");
+        if (Field.class != fieldAttribute.getClass()) {
+            currentSession.invalidate();
+            throw new RuntimeException("Session is broken, try one more time");
+        }
+        return (Field) fieldAttribute;
     }
 }
