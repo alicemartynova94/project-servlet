85051ee777039357d971071d8c7b97436c2086b7
alicemartynova94 <alicemartynova94@gmail.ru>
2023-07-05 15:10:14 +0300

    format code

--- src/main/java/com/tictactoe/InitServlet.java
+++ src/main/java/com/tictactoe/InitServlet.java
@@ -15,15 +15,13 @@ public class InitServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession currentSession = req.getSession(true);
-
         Field field = new Field();
         Map<Integer, Sign> fieldData = field.getField();
-
         List<Sign> data = field.getFieldData();
 
         currentSession.setAttribute("field", field);
         currentSession.setAttribute("data", data);
-        
+
         getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
     }
 }
